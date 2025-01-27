5a0743612bc404cea22b7e3c1aeec064fcf50db6
Johnny Jiang <giovannilance94@gmail.com>
2025-01-28 03:35:03 +1030

    chore: configure pm2 and update server listening configuration

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
